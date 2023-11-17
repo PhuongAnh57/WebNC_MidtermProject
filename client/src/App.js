5ad21e2041387102079a17ec19464a3ffe0afd49
20120224 <mytrinh07112002@gmail.com>
2023-11-18 00:08:19 +0700

    tao trang landing, home, editaccount, login, signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import { react } from 'react';
+import Header from './components/Header';
+import Home from './pages/Home'
+import LogIn from './pages/LogIn'
+import SignUp from './pages/SignUp';
+import EditAccount from './pages/EditAccount';
+import Footer from './components/Footer';
 
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            {/* <Header/> */}
+            {/* <Home/> */}
+            {/* <LogIn/>
+            <SignUp/> */}
+            <EditAccount/>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App;
