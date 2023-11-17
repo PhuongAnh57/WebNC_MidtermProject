635244d537ea5a0c87f747d443a5c1e03aba1b94
KhanhDuy <bkduy001@gmail.com>
2023-11-17 20:42:01 +0700

    delete get home

--- server/controllers/user.c.js
+++ server/controllers/user.c.js
@@ -1,9 +1,9 @@
 const userM = require('../models/user.m');
 
 exports.postRegister = async (req, res) => {
-    const { fullname, username, password, email, address } = req.body.user;
+    // const { fullname, username, password, email, address } = req.body.user;
 
-    const emailExists = await userM.getUserByEmail(email);
-    const usernameExists = await userM.getUserByUsername(username);
+    // const emailExists = await userM.getUserByEmail(email);
+    // const usernameExists = await userM.getUserByUsername(username);
+    res.send('home');
 };
- 
