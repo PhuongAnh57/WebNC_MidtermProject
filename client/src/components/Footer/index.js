883520b5be143d919d87a5d586999623948a4d25
KhanhDuy <bkduy001@gmail.com>
2023-11-19 00:53:03 +0700

    fetch origin

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,33 +7,33 @@ import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="text.secondary">
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function Footer() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh',
-        }}
-      >
-        <CssBaseline />
-        {/* <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    minHeight: '100vh',
+                }}
+            >
+                <CssBaseline />
+                {/* <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
           <Typography variant="h2" component="h1" gutterBottom>
             Sticky footer
           </Typography>
@@ -43,26 +43,22 @@ export default function Footer() {
           </Typography>
           <Typography variant="body1">Sticky footer placeholder.</Typography>
         </Container> */}
-        <Box
-          component="footer"
-          sx={{
-            py: 3,
-            px: 2,
-            mt: 'auto',
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[200]
-                : theme.palette.grey[800],
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography variant="body1">
-              My sticky footer can be found here.
-            </Typography>
-            <Copyright />
-          </Container>
-        </Box>
-      </Box>
-    </ThemeProvider>
-  );
+                <Box
+                    component="footer"
+                    sx={{
+                        py: 3,
+                        px: 2,
+                        mt: 'auto',
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Typography variant="body1">My sticky footer can be found here.</Typography>
+                        <Copyright />
+                    </Container>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
 }
