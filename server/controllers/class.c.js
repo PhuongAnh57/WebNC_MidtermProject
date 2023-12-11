40444c67a668398e5ee28d2be1585114496a82d1
KhanhDuy <bkduy001@gmail.com>
2023-12-11 16:24:13 +0700

    merge with main

--- server/controllers/class.c.js
+++ server/controllers/class.c.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const mailer = require('../utils/mailer');
 const invitationM = require('../models/invitation.m');
 const classM = require('../models/class.m');
-const userM = require('../models/user.m');
 
 exports.getAllClasses = async (req, res) => {
     const authHeader = req.headers['authorization'];
@@ -23,17 +22,17 @@ exports.getAllClasses = async (req, res) => {
 
     try {
         const classes = await classM.getAllClasses();
-        const hasCourse = classes.find(c => c.lecturer_id === userID);
+        const hasCourse = classes.find((c) => c.lecturer_id === userID);
         if (!hasCourse) {
             res.json({ message: 'User does not have any courses' });
         } else {
-            const classesData = classes.filter(c => c.lecturer_id === userID);
+            const classesData = classes.filter((c) => c.lecturer_id === userID);
             res.json({ message: 'Classes', classesData });
         }
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 exports.postCreateClass = async (req, res) => {
     const { name, part, topic, room } = req.body.classInfo;
@@ -64,12 +63,11 @@ exports.postCreateClass = async (req, res) => {
             class_name: name,
             part,
             topic,
-            room
-        }
+            room,
+        };
         // console.log(newClass);
         const nClass = await classM.addNewClass(newClass);
-        
-        
+
         //add lecturer to table class_members
         const class_members = await classM.getAllClass_Members();
         let id;
@@ -83,8 +81,8 @@ exports.postCreateClass = async (req, res) => {
             id,
             class_id,
             member_id: lecturer_id,
-            role: 'teacher'
-        }
+            role: 'teacher',
+        };
         const nClass_Lecturer = await classM.addNewClass_Member(newClass_Lecturer);
         res.json({ message: 'class_id', class_id });
     } catch (error) {
@@ -92,7 +90,17 @@ exports.postCreateClass = async (req, res) => {
     }
 };
 
+exports.getClassDetail = async (req, res) => {
+    const classID = req.params.classID;
 
+    try {
+        const classes = await classM.getAllClasses();
+        const Class = classes.find((c) => c.class_id === +classID);
+        res.json({ message: 'class-detail', Class });
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 // invite students
 
--- server/router/router.r.js
+++ server/router/router.r.js
@@ -29,6 +29,6 @@ router.post('/api/class/add-member', passport.authenticate('jwt', { session: fal
 //-------------------------Class Manager------------------------------------------------------------------------
 router.get('/api/all-classes', passport.authenticate('jwt', { session: false }), classController.getAllClasses);
 router.post('/api/create-class', passport.authenticate('jwt', { session: false }), classController.postCreateClass);
-router.get('/api/class/:classID', classController.getClassDetail);
+router.get('/api/class/:classID', passport.authenticate('jwt', { session: false }), classController.getClassDetail);
 
 module.exports = router;
